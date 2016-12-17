8e349ae3f19295d20b39ebe853e69f6da42e2d19
Alexander <granchar@bigmir.net>
2016-12-17 17:35:26 +0200

    Reduce middle area reset function on the 28 strings of code

--- js/modules/cReset.js
+++ js/modules/cReset.js
@@ -1,40 +1,7 @@
 define(
     $('.rbtn').on('click', function(){
-        var rcl = function(){
-            return [
-                $('.wBread').val(''),
-                $('.bBread').val(''),
-
-                $('.buckwheat').val(''),
-                $('.rise').val(''),
-                $('.oatmeal').val(''),
-                $('.wheat').val(''),
-                $('.peas').val(''),
-                $('.pasta').val(''),
-
-                $('.breast').val(''),
-                $('.liver').val(''),
-                $('.heart').val(''),
-                $('.egg').val(''),
-                $('.wing').val(''),
-
-                $('.potato').val(''),
-                $('.tomato').val(''),
-                $('.cucumber').val(''),
-                $('.cabage').val(''),
-                $('.onion').val(''),
-
-                $('.peasS').val(''),
-                $('.cabageS').val(''),
-                $('.chickenS').val(''),
-                $('.borscht').val(''),
-
-                $('.protein').prop('value', ''),
-                $('.fat').prop('value', ''),
-                $('.carbohydrate').prop('value', ''),
-                $('.kcals').prop('value', '')
-            ];
-        };
-        rcl();
+     $('#mjumbo input').each(function(){
+      $(this).val('');
+     })
     })
 );
